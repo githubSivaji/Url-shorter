47d4639f82d8f7d1daafc22bc73ecd369851d8f2
githubSivaji <123107544+githubSivaji@users.noreply.github.com>
2025-01-23 13:50:24 +0530

    regiser_page

--- URL-Shorter-React/src/App.jsx
+++ URL-Shorter-React/src/App.jsx
@@ -4,6 +4,7 @@ import Landingpage from "./components/Landingpage"
 import AboutPage from "./components/AboutPage"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import RegisterPage from "./components/RegisterPage"
 export default function App() {
   return (
       <BrowserRouter>
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landingpage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
       </Routes>
       <Footer/>
       </BrowserRouter>
